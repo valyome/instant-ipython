ee381c3eb7fe93640d6f64c237feab9f6fd81071
Valyo <valyome@users.noreply.github.com>
2017-04-02 15:43:04 -0300

    Create jupyter_notebook_config.py
    Making Jupyter use PostgresSQL for storage

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,17 @@
+import os
+from pgcontents import PostgresContentsManager
+c = get_config()
+
+# Tell IPython to use PostgresContentsManager for all storage.
+c.NotebookApp.contents_manager_class = PostgresContentsManager
+
+# Set the url for the database used to store files.  See
+# http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html#postgresql
+# for more info on db url formatting.
+c.PostgresContentsManager.db_url = os.environ.get('PGCONTENTS_DB_URL', '')
+
+# PGContents associates each running notebook server with a user, allowing
+# multiple users to connect to the same database without trampling each other's
+# notebooks. By default, we use the result of result of getpass.getuser(), but
+# a username can be specified manually like so:
+c.PostgresContentsManager.user_id = 'default_user'
